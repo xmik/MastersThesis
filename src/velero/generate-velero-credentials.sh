15e2f3ba780fc68801f8c4ff02e66a15150e8a12
xmik <ewa@kudulab.io>
2020-06-07 00:31:28 +0200

    backup k8s cluster

--- src/velero/generate-velero-credentials.sh
+++ src/velero/generate-velero-credentials.sh
@@ -7,13 +7,13 @@ if [[ "${K8S_EXP_ENVIRONMENT}" == "" ]]; then
   echo "Error: K8S_EXP_ENVIRONMENT not set"
   exit 1
 fi
-if [[ "${K8S_EXP_KOPS_S3_BUCKET}" == "" ]]; then
-  echo "Error: K8S_EXP_KOPS_S3_BUCKET not set"
+if [[ "${K8S_EXP_VELERO_S3_BUCKET}" == "" ]]; then
+  echo "Error: K8S_EXP_VELERO_S3_BUCKET not set"
   exit 1
 fi
 
-aws iam create-user --user-name velero-${K8S_EXP_ENVIRONMENT}
-cat > velero-policy-${K8S_EXP_ENVIRONMENT}.json <<EOF
+aws iam create-user --user-name velero-${K8S_EXP_CLUSTER_NAME}
+cat > velero-policy-${K8S_EXP_CLUSTER_NAME}.json <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -39,7 +39,7 @@ cat > velero-policy-${K8S_EXP_ENVIRONMENT}.json <<EOF
                 "s3:ListMultipartUploadParts"
             ],
             "Resource": [
-                "arn:aws:s3:::${K8S_EXP_KOPS_S3_BUCKET}/*"
+                "arn:aws:s3:::${K8S_EXP_VELERO_S3_BUCKET}/*"
             ]
         },
         {
@@ -48,17 +48,17 @@ cat > velero-policy-${K8S_EXP_ENVIRONMENT}.json <<EOF
                 "s3:ListBucket"
             ],
             "Resource": [
-                "arn:aws:s3:::${K8S_EXP_KOPS_S3_BUCKET}"
+                "arn:aws:s3:::${K8S_EXP_VELERO_S3_BUCKET}"
             ]
         }
     ]
 }
 EOF
 aws iam put-user-policy \
-  --user-name velero-${K8S_EXP_ENVIRONMENT} \
-  --policy-name velero-${K8S_EXP_ENVIRONMENT} \
-  --policy-document file://velero-policy-${K8S_EXP_ENVIRONMENT}.json
-json_output=$(aws iam create-access-key --user-name velero-${K8S_EXP_ENVIRONMENT})
+  --user-name velero-${K8S_EXP_CLUSTER_NAME} \
+  --policy-name velero-${K8S_EXP_CLUSTER_NAME} \
+  --policy-document file://velero-policy-${K8S_EXP_CLUSTER_NAME}.json
+json_output=$(aws iam create-access-key --user-name velero-${K8S_EXP_CLUSTER_NAME})
 
 # example json_output looks like this:
 # {
@@ -73,5 +73,11 @@ json_output=$(aws iam create-access-key --user-name velero-${K8S_EXP_ENVIRONMENT
 
 VELERO_ACCESS_KEY_ID=$(echo "$json_output" | jq -r ".AccessKey.AccessKeyId")
 VELERO_SECRET_ACCESS_KEY=$(echo "$json_output" | jq -r ".AccessKey.SecretAccessKey")
-export VELERO_ACCESS_KEY_ID
-export VELERO_SECRET_ACCESS_KEY
+echo "VELERO_ACCESS_KEY_ID: ${VELERO_ACCESS_KEY_ID}"
+echo "VELERO_SECRET_ACCESS_KEY: ${VELERO_SECRET_ACCESS_KEY}"
+cat > credentials-velero.json <<EOF
+[default]
+aws_access_key_id=${VELERO_ACCESS_KEY_ID}
+aws_secret_access_key=${VELERO_SECRET_ACCESS_KEY}
+EOF
+echo "Credentials file created: ${PWD}/credentials-velero.json"
