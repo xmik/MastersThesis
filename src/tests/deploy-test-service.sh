4def22812427827dce39348732f4a0d67a3d1783
xmik <ewa@kudulab.io>
2020-06-07 21:41:04 +0200

    eksctl ca

--- src/tests/deploy-test-service.sh
+++ src/tests/deploy-test-service.sh
@@ -20,12 +20,16 @@ else
   set -x
   kubectl apply -f config-map-www-contents.yaml
   helm repo add bitnami https://charts.bitnami.com/bitnami
-  helm install "${chart_release_name}" -f apache-chart-config.yaml --namespace="${custom_namespace}" --wait --atomic bitnami/apache
-  # # without the following line, we cannot curl the service. The exact error is:
-  # # $ curl -L http://acf690c83d7924db2bed5cdb587525c9-529110400.eu-west-1.elb.amazonaws.com
-  # # curl: (6) Could not resolve host: acf690c83d7924db2bed5cdb587525c9-529110400.eu-west-1.elb.amazonaws.com
-  # helm upgrade "${chart_release_name}" -f apache-chart-config.yaml --namespace="${custom_namespace}" --wait --atomic bitnami/apache
-  # set -x
+  set +e
+  # this command may exit with error, e.g.
+  # Error: release apache-testing failed, and has been uninstalled due to atomic being set: services "apache-testing" not found;
+  # also, do not use --atomic deliberately, if the service is missing, it will be installed later
+  # with helm upgrade
+  helm install "${chart_release_name}" -f apache-chart-config.yaml --namespace="${custom_namespace}" --wait bitnami/apache
+  # without the service installed, we cannot curl the service. The exact error is:
+  # $ curl -L http://acf690c83d7924db2bed5cdb587525c9-529110400.eu-west-1.elb.amazonaws.com
+  # curl: (6) Could not resolve host: acf690c83d7924db2bed5cdb587525c9-529110400.eu-west-1.elb.amazonaws.com
+  set -e
 fi
 
 # sometimes k8s service resource is not created; so try again
